47de7150fe6c85e7588abf2dbf9df0a085ea838f
Praveen Kumar <praveen.kumar@infyair.com>
2025-11-15 16:01:31 +0530

    modify minor function in signUp with google

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -180,7 +180,7 @@ const Signup: React.FC = () => {
               <Link to="/google">
               <button
                 type="button"
-                // onClick={handleGoogleSignup}
+                onClick={handleGoogleSignup}
                 className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
               >
                 <svg className="w-5 h-5" viewBox="0 0 24 24">
